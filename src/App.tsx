f9cd189d6254a848ae2b01558f0348d519d9fcc3
Samandar Jumanov <samandarjumanov055@gmail.com>
2024-01-01 10:16:44 -0500

    adding funcions

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import * as functions from './function'
+
+
+interface  Data  {
+  Id : number ,
+  productCaseName : string ,
+   adminId : number 
+};
 
 
 const App =  ()  => {
+  const [allProductType , setAllProductType ] = useState<any>([]);
+   useEffect(()=>{
+        const data  =  functions.getAllProductTypes()
+        setAllProductType(data)
+   } , []);
+
+
      return (
       <div className="main">
-
-
+        {allProductType.map((each : Data ) =>{
+          return (
+              <div key={each.Id}> 
+                  <h1>{each.productCaseName}</h1>
+                   <h1>{each.adminId}</h1>
+              </div>
+          )
+        })}
 
       </div>
      )
--- src/function.tsx
+++ src/function.tsx
@@ -1,23 +1,44 @@
 import axios from 'axios';
 import {urls} from './urls'
 
-export const getAllProductTypes =  async () =>{
-    const response = await axios.get(urls.allProductCases)
-    console.log(response.data)
+
+export const getAllProductTypes =  async ()  =>{
+     try{
+        const response = await axios.get(urls.allProductCases)
+        return response.data;
+     }catch(err : any ){
+          return { err : err.message}
+     }
 };
 
+
 export const deleteProductCase = async () =>{
+  try {
     const response = await axios.get(urls.deleteProductCase)
-    console.log(response.data)
+    return response.data.message 
+  } catch (error : any ) {
+        return {error : error.message}
+  }
 }
 
 export const createProductCase = async () =>{
-    const response = await axios.get(urls.createProductCase)
-    console.log(response.data)
+       try{
+        const response = await axios.get(urls.deleteProductCase)
+        return response.data.message 
+       }catch(err : any ){
+           return { err : err.message}
+       }
 }
 
 export const updateProductCase = async () =>{
-    const response = await axios.get(urls.updateProductCase)
-    console.log(response.data)
+     try {
+       const response = await axios.get(urls.updateProductCase)
+        return response.data.message 
+     } catch (error : any ) {
+            return { err : error.message }
+     }   
 }
+
+
+
 
